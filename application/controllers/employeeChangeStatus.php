a72768298b7380043f8a6acccfd409e418c66e76
baklavinho <dusan.savic.si@gmail.com>
2017-06-11 00:57:10 +0200

    moje staro

---
+++ application/controllers/employeeChangeStatus.php
@@ -0,0 +1,52 @@
+<?php
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+// if(! $_SESSION) {
+//  session_start(); //we need to call PHP's session object to access it through CI
+// }
+class EmployeeChangeStatus extends CI_Controller
+{
+    
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('employee', '', TRUE);
+    }
+    
+    function index()
+    {
+        if ($this->session->userdata('logged_in')) {
+            $session_data     = $this->session->userdata('logged_in');
+            $data['username'] = $session_data['username'];
+            // $data['h'] = $this->user->selectSpecs();
+
+            // $this->load->view('home_view', $data);
+            $this->load->database();
+            $this->load->model('employee');
+            $data['pendingOrders'] = $this->employee->getPendingOrders();
+            $this->load->view('header.php');
+            $this->load->view('navbar.php', $data);
+            $this->load->view('employeePendingOrders.php', $data); 
+            $this->load->view('footer.php');
+        } else {
+            //If no session, redirect to login page
+            $this->load->helper('url');
+            redirect('login', 'refresh');
+            
+        }
+    }
+    
+
+    	function changeStatus()
+    {
+            if(isset($_POST["order_status"]) && isset($_POST["order_id"])) 	
+            {
+                    $this->employee->changeStatus($_POST["order_status"], $_POST["order_id"]);
+                    redirect("EmployeeChangeStatus", "refresh");
+            }
+            
+    }
+
+    }
+
---
+++ application/views/employeePendingOrders.php
@@ -0,0 +1,63 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+ 
+ <section id="contact" class="section-scroll main-section">
+		<header class="section-header">
+			<h2>Apetit Restaurant</h2>
+		</header> 
+	<div class="contact-content container section-padding">
+		<div class="row">
+			<div class="col-xs-12 col-md-5 v-card">
+				
+				<div class="row">
+					
+				<table style="width:100%">
+				  <tr>
+				    <th>Order Date</th>
+				    <th>Specialty</th>
+				    <th>Amount</th>
+				    <th>Status</th>
+				    <th>Action</th>
+				  </tr>
+				  <?php  
+         		foreach ($pendingOrders->result() as $row) 
+         		{ 
+            	?>
+            		<tr>
+            		<form action="employeeChangeStatus/changeStatus" method="POST">
+				  	
+				  		<td><?php echo $row->date;?></td>
+					  	<td><?php echo $row->name;?></td>
+					    <td><?php echo $row->price;?></td>
+					    <td><?php echo $row->order_status;?></td>
+					    <td><select name="order_status">
+		  					<option <?php if ($row->order_status == "Pending") echo "Accept" ?> value="Accepted">Accept</option>
+		  					<option <?php if ($row->order_status == "Pending") echo "Accept" ?> value="Rejected">Reject</option>
+							</select> 
+						</td>
+						<input type="hidden" name="order_id" value="<?php echo $row->order_id;?>"><br>
+					    <td><button class="btn btn-default btn-sm" type="submit">Change</button></td>
+				    	</form>
+				
+				 	</tr>
+
+				<?php
+				}
+				?>
+				</table> 
+				
+				</br>
+				</br>
+				
+
+
+						
+				</div>
+					
+
+				</div>
+			</div>
+			
+		</div>
+	</div>
+</section>
